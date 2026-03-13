616c053c94c2d97f90c4f6dc966d1ff489d536c1
Prithivi <prithivi@users.noreply.github.com>
2026-03-13 20:06:59 +0545

    feat: enhance chair configurator with color and leg options

--- src/components/chair-configuration/Chair.tsx
+++ src/components/chair-configuration/Chair.tsx
@@ -3,6 +3,7 @@ import type { ThreeElements } from "@react-three/fiber";
 import type { GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
 import type { BufferGeometry, MeshStandardMaterial } from "three";
 import * as Three from "three";
+import { useCustomization } from "../../context/Customization";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -19,36 +20,38 @@ type GLTFResult = GLTF & {
 };
 
 const Chair = (props: ThreeElements["group"]) => {
+  const { material, legs, chairColor, cushionColor } = useCustomization();
+
   const { nodes, materials } = useGLTF(
     "/models/chair.gltf",
   ) as unknown as GLTFResult;
 
   const leatherTextureProps = useTexture({
-    map: "/textures/leather/Leather_008_Base Color.jpg",
+    // map: "/textures/leather/Leather_008_Base Color.jpg",
     normalMap: "/textures/leather/Leather_008_Normal.jpg",
     roughnessMap: "/textures/leather/Leather_008_Roughness.jpg",
     aoMap: "/textures/leather/Leather_008_Ambient Occlusion.jpg",
   });
 
   const fabricTextureProps = useTexture({
-    map: "/textures/fabrics/Fabric_Knitted_006_basecolor.jpg",
+    // map: "/textures/fabrics/Fabric_Knitted_006_basecolor.jpg",
     normalMap: "/textures/fabrics/Fabric_Knitted_006_normal.jpg",
     roughnessMap: "/textures/fabrics/Fabric_Knitted_006_roughness.jpg",
     aoMap: "/textures/fabrics/Fabric_Knitted_006_ambientOcclusion.jpg",
   });
 
-  leatherTextureProps.map.repeat.set(3, 3);
+  //   leatherTextureProps.map.repeat.set(3, 3);
   leatherTextureProps.normalMap.repeat.set(3, 3);
   leatherTextureProps.roughnessMap.repeat.set(3, 3);
   leatherTextureProps.aoMap.repeat.set(3, 3);
 
-  fabricTextureProps.map.repeat.set(3, 3);
+  //   fabricTextureProps.map.repeat.set(3, 3);
   fabricTextureProps.normalMap.repeat.set(3, 3);
   fabricTextureProps.roughnessMap.repeat.set(3, 3);
   fabricTextureProps.aoMap.repeat.set(3, 3);
 
-  leatherTextureProps.map.wrapS = leatherTextureProps.map.wrapT =
-    Three.RepeatWrapping;
+  //   leatherTextureProps.map.wrapS = leatherTextureProps.map.wrapT =
+  //     Three.RepeatWrapping;
   leatherTextureProps.normalMap.wrapS = leatherTextureProps.normalMap.wrapT =
     Three.RepeatWrapping;
   leatherTextureProps.roughnessMap.wrapS =
@@ -56,8 +59,8 @@ const Chair = (props: ThreeElements["group"]) => {
   leatherTextureProps.aoMap.wrapS = leatherTextureProps.aoMap.wrapT =
     Three.RepeatWrapping;
 
-  fabricTextureProps.map.wrapS = fabricTextureProps.map.wrapT =
-    Three.RepeatWrapping;
+  //   fabricTextureProps.map.wrapS = fabricTextureProps.map.wrapT =
+  //     Three.RepeatWrapping;
   fabricTextureProps.normalMap.wrapS = fabricTextureProps.normalMap.wrapT =
     Three.RepeatWrapping;
   fabricTextureProps.roughnessMap.wrapS =
@@ -68,7 +71,12 @@ const Chair = (props: ThreeElements["group"]) => {
   return (
     <group {...props} dispose={null}>
       <mesh castShadow receiveShadow geometry={nodes.Chair.geometry}>
-        <meshStandardMaterial {...leatherTextureProps} />
+        <meshStandardMaterial
+          {...(material === "leather"
+            ? leatherTextureProps
+            : fabricTextureProps)}
+          color={chairColor.color}
+        />
       </mesh>
       <mesh
         castShadow
@@ -76,19 +84,24 @@ const Chair = (props: ThreeElements["group"]) => {
         geometry={nodes.Cushion.geometry}
         position={[0, 0.064, 0.045]}
       >
-        <meshStandardMaterial {...fabricTextureProps} />
+        <meshStandardMaterial
+          {...fabricTextureProps}
+          color={cushionColor.color}
+        />
       </mesh>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Legs1.geometry}
         material={materials.Legs}
+        visible={legs === 1}
       />
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Legs2.geometry}
         material={materials.Legs}
+        visible={legs === 2}
       />
     </group>
   );
--- src/context/Customization.tsx
+++ src/context/Customization.tsx
@@ -1,10 +1,94 @@
 import React, { createContext, useContext, useState } from "react";
+type ChairColorOption = {
+  color: string;
+  name: string;
+};
+
+type CushionColorOption = {
+  color: string;
+  name: string;
+};
+
+export const chairColors: ChairColorOption[] = [
+  {
+    color: "#683434",
+    name: "brown",
+  },
+  {
+    color: "#1a5e1a",
+    name: "green",
+  },
+  {
+    color: "#659994",
+    name: "blue",
+  },
+  {
+    color: "#896599",
+    name: "mauve",
+  },
+  {
+    color: "#ffa500",
+    name: "orange",
+  },
+  {
+    color: "#59555b",
+    name: "grey",
+  },
+  {
+    color: "#222222",
+    name: "black",
+  },
+  {
+    color: "#ececec",
+    name: "white",
+  },
+];
+export const cushionColors: CushionColorOption[] = [
+  {
+    color: "#683434",
+    name: "brown",
+  },
+  {
+    color: "#1a5e1a",
+    name: "green",
+  },
+  {
+    color: "#659994",
+    name: "blue",
+  },
+  {
+    color: "#896599",
+    name: "mauve",
+  },
+  {
+    color: "#ffa500",
+    name: "orange",
+  },
+  {
+    color: "#59555b",
+    name: "grey",
+  },
+  {
+    color: "#222222",
+    name: "black",
+  },
+  {
+    color: "#ececec",
+    name: "white",
+  },
+];
 
 type MaterialType = "leather" | "fabric";
 
 type CustomizationContextType = {
   material: MaterialType;
   setMaterial: React.Dispatch<React.SetStateAction<MaterialType>>;
+  legs: number;
+  setLegs: React.Dispatch<React.SetStateAction<number>>;
+  chairColor: ChairColorOption;
+  setChairColor: React.Dispatch<React.SetStateAction<ChairColorOption>>;
+  cushionColor: CushionColorOption;
+  setCushionColor: React.Dispatch<React.SetStateAction<CushionColorOption>>;
 };
 
 const CustomizationContext = createContext<
@@ -17,12 +101,25 @@ export const CustomizationProvider = ({
   children: React.ReactNode;
 }) => {
   const [material, setMaterial] = useState<MaterialType>("leather");
+  const [legs, setLegs] = useState(1);
+  const [chairColor, setChairColor] = useState<ChairColorOption>(
+    chairColors[0],
+  );
+  const [cushionColor, setCushionColor] = useState<CushionColorOption>(
+    cushionColors[0],
+  );
 
   return (
     <CustomizationContext.Provider
       value={{
         material,
         setMaterial,
+        legs,
+        setLegs,
+        chairColor,
+        setChairColor,
+        cushionColor,
+        setCushionColor,
       }}
     >
       {children}
